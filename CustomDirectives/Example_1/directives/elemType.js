ce3c111c7b705338dd36aad3f6dd1029c2348b94
NareshIT <hr@nareshit.in>
2017-08-26 18:52:36 +0530

    Custom Directives

---
+++ CustomDirectives/Example_1/directives/elemType.js
@@ -0,0 +1,6 @@
+app.directive("elemType",elemType);
+function elemType() {
+    return{
+        template:"<div class='jumbotron'><b style='color: green'>Element Type Custom Directive !</b></div>"
+    }
+}
