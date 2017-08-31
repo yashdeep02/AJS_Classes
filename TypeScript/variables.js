0d09aeb0533aa645e3be862622f603cdc92a2c26
nareshit <hr@nareshit.in>
2017-08-31 19:14:29 +0530

    Deleted Old Batch Files !

---
+++ TypeScript/variables.js
@@ -0,0 +1,9 @@
+var data = 100;
+var str = "Data From DataBase....!";
+var flag = true;
+function fun_one(arg1, arg2) {
+    return fun_two(arg1, arg2);
+}
+function fun_two(arg1, arg2) {
+    return arg1 + arg2;
+}
