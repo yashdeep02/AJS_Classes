4e25fcd93efc8ab0b7cad64d17cf9f507c46e819
Samba <samba.pendela@gmail.com>
2017-09-11 19:17:15 +0530

    Single Page Applications

---
+++ Directives/ngModel/NgModelDemo.ts
@@ -0,0 +1,10 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector:"ng-model",
+  templateUrl:"./ng-model.html"
+})
+export class NgModelDemo{
+  model_one:number=0;
+  model_two:number=0;
+}
