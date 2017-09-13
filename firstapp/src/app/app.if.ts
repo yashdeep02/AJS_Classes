a1c89ee96944cb7e92c6864629849775f6d50f19
Samba <samba.pendela@gmail.com>
2017-09-13 19:19:14 +0530

    SPA_Class_Two

---
+++ firstapp/src/app/app.if.ts
@@ -0,0 +1,15 @@
+import {Component} from "@angular/core";
+
+@Component({
+  selector:"app-root",
+  templateUrl:"./if.html"
+})
+export class NgIfDemo{
+    array=[
+      {"e_id":111,"e_name":"Hello_1","e_sal":10000},
+      {"e_id":222,"e_name":"Hello_2","e_sal":100000},
+      {"e_id":333,"e_name":"Hello_3","e_sal":60000},
+      {"e_id":444,"e_name":"Hello_4","e_sal":40000},
+      {"e_id":555,"e_name":"Hello_5","e_sal":30000}
+    ];
+}
