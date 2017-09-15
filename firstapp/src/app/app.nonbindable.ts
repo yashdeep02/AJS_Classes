f9ff73dcd33aa938bb61b699c293e958d5e72369
Samba <samba.pendela@gmail.com>
2017-09-15 18:05:07 +0530

    Deleted Old Files

--- firstapp/src/app/app.nonbindable.ts
+++
@@ -1,8 +0,0 @@
-import {Component} from "@angular/core";
-@Component({
-    selector:"app-root",
-    template:"<h1><pre ngNonBindable>{{data}}</pre></h1>"
-})
-export class NgNonBindableDemo{
-  data="Welcome";
-}
--- firstapp/src/app/app.style.ts
+++
@@ -1,9 +0,0 @@
-import {Component} from "@angular/core";
-
-@Component({
-  selector:"app-root",
-  template:"<ng-style></ng-style><ng-class></ng-class>"
-})
-export class Style{
-
-}
