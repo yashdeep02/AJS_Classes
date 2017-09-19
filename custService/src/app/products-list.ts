e20a672c606e9f71a496d7564917db57886e07d4
Samba <samba.pendela@gmail.com>
2017-09-19 19:17:21 +0530

    Services

---
+++ custService/src/app/products-list.ts
@@ -0,0 +1,17 @@
+import {Products} from "./products";
+export const PRODUCTS:Products[]=[
+  /*new Products(111,'p_one',10000),
+  new Products(222,'p_two',20000),
+  new Products(333,'p_three',30000),
+  new Products(444,'p_four',40000),
+  new Products(555,'p_five',50000)*/
+
+  /*{'p_id':111,'p_name':'p_one','p_cost':10000},
+  {'p_id':222,'p_name':'p_two','p_cost':20000},
+  {'p_id':333,'p_name':'p_three','p_cost':30000},
+  {'p_id':444,'p_name':'p_four','p_cost':40000},
+  {'p_id':555,'p_name':'p_five','p_cost':50000}*/
+
+  {'p_id':111,'p_name':'p_one','p_cost':10000},
+  new Products(222,'p_two',20000)
+];
---
+++ serviceExample/src/app/products.ts
@@ -0,0 +1,6 @@
+export class products{
+  constructor(public p_id:number,
+              public p_name:string,
+              public p_cost:number){
+  }
+}
